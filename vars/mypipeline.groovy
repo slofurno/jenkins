9e423bf62395fde426160ab104472cf0f23b2d19
Steve LoFurno <slofurno@gmail.com>
2019-04-04 18:26:10 +0000

    ok

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -10,20 +10,22 @@ def call(Map params = [:]) {
     agent any
     stages {
       stage("hello") {
+
+        environment {
+          NAME = params["name"]
+        }
+
         steps {
           echo params["name"]
           withCredentials([string(credentialsId: 'mysecret', variable: 'MYSECRET')]) {
             script {
               sh "echo mysecret: $MYSECRET"
-              def name = params["name"]
               params["install"].each {
                 sh it
               }
 
-              sh 'echo "project name: ${name}"'
-
               sh '''
-                echo "project name: ${name}"
+                echo "project name: ${NAME}"
               '''
             }
           }
